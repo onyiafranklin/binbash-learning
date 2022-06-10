5da36c56646c153f5ba9c9cbd521fc35ca00892b
onyiafranklin <onyiafranklin89@gmail.com>
2022-06-10 23:45:06 +0100

    first commit

---
+++ helloscript.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+##### commenting
+# cat << keto
+# here is the way to go for it
+# oya naa
+# keto
+#cat >> fis.txt
+#####conditional
+count=14
+# if [ $count -lt 1 ]
+# then
+#         echo "the condition is true"
+
+# else 
+#         echo "the condition is false"
+# fi
+# conditional 2
+if (( $count >= 11 ))
+then
+        echo "the condition is true"
+
+else 
+        echo "the condition is false"
+fi
+
+if (( $count >= 11 ))
+then
+        echo "the condition is true"
+
+else 
+        echo "the condition is false"
+fi
